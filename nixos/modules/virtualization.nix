b84dbdc7e2dee1aeaacf8175d065ab716fb274b2
Mario Marín <mmfmarin@gmail.com>
2025-09-10 15:49:57 -0500

    fix: disable rootless Docker configuration
    - Remove rootless Docker configuration
    - Keep standard Docker daemon enabled
    - Remove commented Podman configuration
    - Docker will now use system socket at /var/run/docker.sock

--- nixos/modules/virtualization.nix
+++ nixos/modules/virtualization.nix
@@ -3,17 +3,7 @@
 {
   # Docker
   virtualisation.docker.enable = true;
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-  };
 
   # Libvirt/QEMU
   virtualisation.libvirtd.enable = true;
-
-  # Alternative: Podman (commented out)
-  # virtualisation.podman.enable = true;
-  # virtualisation.podman.dockerCompat = true;
-  # virtualisation.podman.dockerSocket.enable = true;
-  # virtualisation.podman.defaultNetwork.dnsname.enable = true;
 }
