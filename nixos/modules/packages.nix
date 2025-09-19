25f6559a279e556ea5797658142bc98bc409d77b
Mario Marín <mmfmarin@gmail.com>
2025-09-19 15:34:29 -0500

    feat(nixos): add Obsidian to desktop applications
    - Add Obsidian knowledge base and note-taking app to packages list
    - Placed alphabetically between gnome-disk-utility and transmission-gtk

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -24,6 +24,7 @@
     firefox # Open-source web browser
     gimp # GNU Image Manipulation Program
     gnome-disk-utility # Disk management utility
+    obsidian # Knowledge base and note-taking application
     transmission-gtk # BitTorrent client with GTK interface
     zathura # PDF/document viewer
     zoom-us # Video conferencing software
