9194350878490b6b641f716d334a533d7bbc2adb
Mario Marín <mmfmarin@gmail.com>
2025-11-14 13:17:04 -0500

    refactor(nixos): make boot and networking conditional for WSL
    - Boot module: Skip bootloader, kernel modules, and Plymouth for WSL
    - Networking module: Skip NetworkManager and dnscrypt for WSL
    
    WSL uses Windows networking stack and doesn't support traditional
    boot configuration. Both modules now check custom.wsl.enable.

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,30 +1,33 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Boot configuration - only for non-WSL systems
+  config = lib.mkIf (!config.custom.wsl.enable or false) {
+    # Bootloader
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
+    # Setup keyfile
+    boot.initrd.secrets = {
+      "/crypto_keyfile.bin" = null;
+    };
 
-  # Kernel modules
-  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "thinkpad_acpi" ];
-  boot.initrd.kernelModules = [ "acpi_call" "kvm-intel" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    acpi_call
-  ] ++ [
-    pkgs.linuxPackages.sysdig
-  ];
+    # Kernel modules
+    boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "thinkpad_acpi" ];
+    boot.initrd.kernelModules = [ "acpi_call" "kvm-intel" ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+      acpi_call
+    ] ++ [
+      pkgs.linuxPackages.sysdig
+    ];
 
-  # Plymouth boot splash
-  boot.plymouth = {
-    enable = true;
-    themePackages = [ (pkgs.adi1090x-plymouth-themes.override { selected_themes = [ "owl" ]; }) ];
-    theme = "owl";
+    # Plymouth boot splash
+    boot.plymouth = {
+      enable = true;
+      themePackages = [ (pkgs.adi1090x-plymouth-themes.override { selected_themes = [ "owl" ]; }) ];
+      theme = "owl";
+    };
   };
 }
--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -1,48 +1,52 @@
 { config, pkgs, lib, ... }:
 
 {
-  networking.hostName = "nixos";
+  # Networking configuration - only for non-WSL systems
+  # WSL uses Windows networking stack
+  config = lib.mkIf (!config.custom.wsl.enable or false) {
+    networking.hostName = "nixos";
 
-  networking = {
-    nameservers = [ "127.0.0.1" "::1" ];
-    networkmanager.enable = true;
-    networkmanager.dns = "none";
+    networking = {
+      nameservers = [ "127.0.0.1" "::1" ];
+      networkmanager.enable = true;
+      networkmanager.dns = "none";
 
-    firewall = {
-      checkReversePath = false;
-      allowedTCPPorts = [
-        443
-        8081
-        6443
-        19000
-        19001
-      ];
-    };
-  };
-
-  # DNS over HTTPS
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      ipv6_servers = true;
-      require_dnssec = true;
-
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+      firewall = {
+        checkReversePath = false;
+        allowedTCPPorts = [
+          443
+          8081
+          6443
+          19000
+          19001
         ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
     };
-  };
 
-  systemd.services.dnscrypt-proxy2.serviceConfig = {
-    StateDirectory = "dnscrypt-proxy";
-  };
+    # DNS over HTTPS
+    services.dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
 
-  # Captive portal browser
-  programs.captive-browser.enable = true;
-  programs.captive-browser.interface = "wlp4s0";
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+      };
+    };
+
+    systemd.services.dnscrypt-proxy2.serviceConfig = {
+      StateDirectory = "dnscrypt-proxy";
+    };
+
+    # Captive portal browser
+    programs.captive-browser.enable = true;
+    programs.captive-browser.interface = "wlp4s0";
+  };
 }
