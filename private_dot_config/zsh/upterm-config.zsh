d91d8bec4e74bd2108e7e47b92e6eb23f489d3f5
Mario Marín <mmfmarin@gmail.com>
2025-12-02 15:35:58 -0500

    refactor: move upterm config to .zshenv
    - Add UPTERM_SERVER to .zshenv instead of separate config file
    - Remove upterm-config.zsh (unnecessary complexity)
    - Remove sourcing line from .zshrc

--- private_dot_config/zsh/upterm-config.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-# Upterm configuration for secure terminal sharing
-
-# Use WebSocket protocol for better firewall compatibility
-# This works better through corporate firewalls that may block SSH port 22
-export UPTERM_SERVER=wss://uptermd.upterm.dev
