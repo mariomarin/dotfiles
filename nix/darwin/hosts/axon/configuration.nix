5e90a659fb8958ff4c29227c7cca248a82d5f359
Mario Marín <mmfmarin@gmail.com>
2026-05-17 09:54:53 -0500

    [spr] changes to main this commit is based on
    Created using jj-spr 0.1.0
    
    [skip ci]

--- nix/darwin/hosts/axon/configuration.nix
+++ nix/darwin/hosts/axon/configuration.nix
@@ -3,13 +3,16 @@
 
 let
   username = userConfig.username;
+  homeDir = "/Users/${username}";
 in
 {
   imports = [
     ../../../common/modules/cli-tools.nix
     ../../../common/modules/fonts.nix
     ../../../common/modules/tmux.nix
+    ../../modules/apps.nix
     ../../modules/xdg-open-svc.nix
+    ../../modules/kanata.nix
   ];
 
   custom.cli = {
@@ -22,6 +25,31 @@ in
   nix.enable = false;
 
   programs.zsh.enableGlobalCompInit = false;
+
+  # Clipper - clipboard daemon for remote access
+  launchd.user.agents.clipper = {
+    serviceConfig = {
+      Label = "org.local.clipper";
+      ProgramArguments = [ "${pkgs.clipper}/bin/clipper" "--address" "127.0.0.1" "--port" "8377" ];
+      KeepAlive = true;
+      RunAtLoad = true;
+      ProcessType = "Background";
+    };
+  };
+
+  # Syncthing - file synchronization
+  launchd.user.agents.syncthing = {
+    serviceConfig = {
+      Label = "org.syncthing.syncthing";
+      ProgramArguments = [ "${pkgs.syncthing}/bin/syncthing" "-no-browser" "-no-restart" ];
+      KeepAlive = true;
+      RunAtLoad = true;
+      ProcessType = "Background";
+      StandardOutPath = "${homeDir}/Library/Logs/syncthing.log";
+      StandardErrorPath = "${homeDir}/Library/Logs/syncthing.log";
+    };
+  };
+
 
   system.stateVersion = 4;
 }
