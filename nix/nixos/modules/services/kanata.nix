924ad122b2b080d18be686cdf0e01ca2c1f3fbb6
Mario Marín <mmfmarin@gmail.com>
2026-05-19 12:09:54 -0500

    [jj-spr] initial version
    Created using jj-spr 0.1.0

--- nix/nixos/modules/services/kanata.nix
+++ nix/nixos/modules/services/kanata.nix
@@ -10,18 +10,21 @@
         # ThinkPad T470 internal keyboard
         devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
         extraDefCfg = "process-unmapped-keys yes\n  danger-enable-cmd no";
+        port = 5829;
         config = builtins.readFile ../../../../private_dot_config/kanata/core.kbd;
       };
       kinesis = {
         # Kinesis Freestyle 2 USB keyboard
         devices = [ "/dev/input/by-id/usb-KINESIS_FREESTYLE_KB800_KB800_Kinesis_Freestyle-event-kbd" ];
         extraDefCfg = "process-unmapped-keys yes\n  danger-enable-cmd no";
+        port = 5830;
         config = builtins.readFile ../../../../private_dot_config/kanata/core.kbd;
       };
       calliope = {
         # Lenovo Calliope USB Keyboard G2
         devices = [ "/dev/input/by-id/usb-LiteOn_Lenovo_Calliope_USB_Keyboard_G2-event-kbd" ];
         extraDefCfg = "process-unmapped-keys yes\n  danger-enable-cmd no";
+        port = 5831;
         config = builtins.readFile ../../../../private_dot_config/kanata/core.kbd;
       };
     };
